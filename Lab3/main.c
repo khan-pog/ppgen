61b1974b88240bbd3f38ea6da0cc451981a5c6fd
khan-pog <62162976+khan-pog@users.noreply.github.com>
2021-08-18 14:45:14 +1000

    Update main.c

--- Lab3/main.c
+++ Lab3/main.c
@@ -6,6 +6,7 @@
 
 #include "pico/stdlib.h"
 #include <stdbool.h>
+#define CONTENT_OF(adrr) (*(volatile uint32_t*)adrr)
 
 
 
@@ -20,19 +21,20 @@ uint32_t* SIO_BASE = 0Xd0000000
 uint32_t* REG_GPIO_OE = (SIO_BASE+0x020)
 uint32_t* REG_GPIO_OE_SET = (SIO_BASE+0x024)
 uint32_t* REG_GPIO_OE_CLR = (SIO_BASE+0x028)
-uint32_t* CONTENT_OF(REG_GPIO_OE_SET) = (*(volatile uint32_t*)REG_GPIO_OE_SET)
-uint32_t* CONTENT_OF(REG_GPIO_OE_CLR) = (*(volatile uint32_t*)REG_GPIO_OE_CLR)
+
 uint32_t LED_PIN = 25
 
 //GPIO25 ouput diabled and enable input 
 *GPIO25PAD_BASE = *GPIO25PAD_BASE | (1<<6) & ~(1<<7); // Sets bit 7 to 0 and set bit 6 to 1
-//GPIO25 GPIO25 functoin set to 0x1f
+//GPIO25 GPIO25 functoin set to SIO
 *GPIO_CTRL = 5; //
+
+// Set the GPIO Output enable as well and that means set bit 25 of the register 
 
 while ( true ) {
 
 //Turn LED ON
-uint32_t* CONTENT_OF(REG_GPIO_OE_SET) =  LED_PIN;
+uint32_t* CONTENT_OF(REG_GPIO_OE_SET) =  (1<<LED_PIN);
 
 //Wait 
 for ( uint32_t i = 0; i < 8000000; i++) {
@@ -40,7 +42,7 @@ __asm volatile ( " nop " ) ;
 }
 
 //Turn LED OFF
-uint32_t* CONTENT_OF(REG_GPIO_OE_CLR) =  LED_PIN;
+uint32_t* CONTENT_OF(REG_GPIO_OE_CLR) =  (1<<LED_PIN);
 
 }
 }
