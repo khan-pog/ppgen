3eb2fd0827e3d621ac4d9a3d573a7c7fb7153875
khan-pog <khanthompson123@gmail.com>
2021-08-19 19:55:41 +1000

    Make all constants #defined

--- Lab3/main.c
+++ Lab3/main.c
@@ -6,23 +6,23 @@
 
 #include "pico/stdlib.h"
 #include <stdbool.h>
-#define CONTENT_OF(adrr) (*(volatile uint32_t*)adrr)
+
 
 
 
 int main ( void ) {
 // TODO − Initialise components and variables
 //enable the pads
-typedef unsigned long uint32_t ;
+#define typedef unsigned long uint32_t 
 
-uint32_t* GPIO25PAD_BASE = 0x40014000
-uint32_t* GPIO_CTRL = (GPIO25PAD_BASE+0x0cc)
-uint32_t* SIO_BASE = 0Xd0000000
-uint32_t* REG_GPIO_OE = (SIO_BASE+0x020)
-uint32_t* REG_GPIO_OE_SET = (SIO_BASE+0x024) // maybe 14
-uint32_t* REG_GPIO_OE_CLR = (SIO_BASE+0x028) // maybe 18
-
-uint32_t LED_PIN = 25
+#define GPIO25PAD_BASE 0x40014000
+#define GPIO_CTRL (GPIO25PAD_BASE+0x0cc)
+#define SIO_BASE_ADDRESS 0Xd0000000
+#define REG_GPIO_OE (SIO_BASE_ADDRESS+0x020) //maybe 24
+#define REG_GPIO_OE_SET (SIO_BASE_ADDRESS+0x024) // maybe 14
+#define REG_GPIO_OE_CLR (SIO_BASE_ADDRESS+0x028) // maybe 18
+#define CONTENT_OF(adrr) (*(volatile uint32_t*)adrr)
+#define LED_PIN 25
 
 //GPIO25 ouput diabled and enable input 
 *GPIO25PAD_BASE = *GPIO25PAD_BASE | (1<<6) & ~(1<<7); // Sets bit 7 to 0 and set bit 6 to 1
@@ -36,7 +36,7 @@ uint32_t* CONTENT_OF(REG_GPIO_OE) =  (1<<LED_PIN);
 while ( true ) {
 
 //Turn LED ON
-uint32_t* CONTENT_OF(REG_GPIO_OE_SET) =  (1<<LED_PIN);
+uint32_t* CONTENT_OF(REG_GPIO_OE_SET) = (1<<LED_PIN);
 
 //Wait 
 for ( uint32_t i = 0; i < 8000000; i++) {
@@ -44,7 +44,7 @@ __asm volatile ( " nop " ) ;
 }
 
 //Turn LED OFF
-uint32_t* CONTENT_OF(REG_GPIO_OE_CLR) =  (1<<LED_PIN);
+uint32_t* CONTENT_OF(REG_GPIO_OE_CLR) = (1<<LED_PIN);
 
 }
 }
